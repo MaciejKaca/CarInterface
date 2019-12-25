abb8b5b69ceb4efa720e9ee33e51d0a2da8cf51b
MaciejKaca <maciek.kaca@gmail.com>
2019-12-25 18:54:01 +0100

    Modified message

--- interface.h
+++ interface.h
@@ -1,32 +1,62 @@
-#include <stdint.h>
-
 #pragma once
+
+#include <stdint.h>
 
 enum Devices
 {
-    BRAKE_LIGHT = 0,
-    REVERSE_LIGHT = 1,
-    LEFT_BLINKER_LIGHT = 2,
-    RIGHT_BLINKER_LIGHT = 3,
+    BRAKE_LIGHT = 1,
+    REVERSE_LIGHT = 2,
+    TURN_SIGNAL = 3,
     HEADLIGHT = 4,
-    SERVO = 5,
-    HAZARD_LIGHTS = 6
+    SERVO = 5
 };
 
-enum Command
+enum ServoCommand
 {
-    OFF = 0,
-    ON = 255,
-    DAYTIME = 10,
-    BRAKE = 255,
-    HIGH_BEAM = 255,
     SET_NEW_ZERO = 1,
     TURN = 2
+};
+
+enum TurnSignalCommand
+{
+    TURN_SIGNAL_OFF = 0,
+    LEFT = 2,
+    RIGHT = 3,
+    HAZARD_LIGHTS = 4
+};
+
+enum HeadLightCommand
+{
+    HEADLIGHT_OFF = 0,
+    HEADLIGHT_DAYTIME = 20,
+    HIGH_BEAM = 255
+};
+
+enum BrakeLightsCommand
+{
+    BRAKE_LIGHT_OFF = 0,
+    BRAKE_LIGHT_DAYTIME = 20,
+    STOP = 255
+};
+
+enum ReverseLightCommand
+{
+    REVERSE_LIGHT_OFF = 0,
+    ON = 20
+};
+
+struct ServoInfo
+{
+    ServoCommand command;
+    int8_t degrees;
 };
 
 struct LightsAndServoMsg
 {
     Devices device;
-    Command command;
-    int8_t degrees;
+    ServoInfo servoInfo;
+    HeadLightCommand headLightCommand;
+    BrakeLightsCommand brakeLightsCommand;
+    ReverseLightCommand reverseLightCommand;
+    TurnSignalCommand turnSignalCommand;
 };
